8858774414b21685b3b688949d11b5cad77b0bf4
Husam Chekfa <77081469+HusamChekfa@users.noreply.github.com>
2025-03-17 18:49:03 -0700

    done with this branch for now, moving to dpll_new

--- Clause.h
+++ Clause.h
@@ -8,7 +8,7 @@ using namespace std;
 
 struct Clause {
     std::vector<int> literals;
-    unsigned int unassigned = 0; // count of unassigned variables // might not be useful if we go for the map BCP
+    unsigned unassigned = 0; // count of unassigned variables // might not be useful if we go for the map BCP
 };
 
 #endif // CLAUSE_H
