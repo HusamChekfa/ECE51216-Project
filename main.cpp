4ea1cc0901b7552f034ed081144b837bf435163d
Husam Chekfa <77081469+HusamChekfa@users.noreply.github.com>
2025-05-04 19:38:54 -0700

    further testing some executables

--- main.cpp
+++ main.cpp
@@ -43,12 +43,12 @@ void check(vector<Clause> checking, const vector<int> & sol) {
         }
     }*/
     if (checking.size() ==  0) {
-        cout << "\t\tSUCCESS !!!! " << endl;
+        //a//cout << "\t\tSUCCESS !!!! " << endl;
         //print_solution(sol);
         ++sat;
     }
     else {
-        cout << "\t\tWRONG SOLUTION!!!!! " << endl;
+        //a//cout << "\t\tWRONG SOLUTION!!!!! " << endl;
         print_solution(sol);
         for (int z = 0; z < checking.size(); ++z) {
             for (int y = 1; y < sol.size(); ++y) {
@@ -83,55 +83,141 @@ void clean_solution(vector<int> & sol) {
 }
 
 void print_solution(const vector<int> & sol) {
-    cout << "\t\tASSIGNMENT:";
+    //a//cout << "\t\tASSIGNMENT:";
     for (int i = 1; i < sol.size(); ++i) {
-        cout << i << "=" << (sol[i] == 1 ? "1 " : "0 ");
+        //a//cout << i << "=" << (sol[i] == 1 ? "1 " : "0 ");
     }
     cout << endl;
 }
 
-int main(int argc, char* argv[]) {
+int main() {
+    std::vector<double> elapsed_seconds;
+    string path = "";// "C:/Users/Husam Study/Documents/GitHub/ECE51216-Project/SAT/uf20-01-small.cnf";
+    //path = "C:/Users/Husam Study/Documents/GitHub/ECE51216-Project/SAT/20v91c1000iAllSat/";
+    //path = "C:/Users/Husam Study/Documents/GitHub/ECE51216-Project/SAT/50v218c1000iAllSat/";
+    //path = "C:/Users/Husam Study/Documents/GitHub/ECE51216-Project/SAT/50v218c1000iAllUnSat/";
+    //path = "C:/Users/Husam Study/Documents/GitHub/ECE51216-Project/SAT/uf20-01-small.cnf";
+    //string path = "C:/Users/Husam Study/Documents/GitHub/ECE51216-Project/SAT/";
+    //path = "C:/Users/Husam Study/Documents/GitHub/ECE51216-Project/SAT/20v91c1000iAllSat/uf20-01.cnf";
+    //path = "C:/Users/Husam Study/Documents/GitHub/ECE51216-Project/SAT/broken.cnf";
 
-    if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " <inputfile.cnf>\n";
-        return 1;
-    }
+    path = "C:/Users/Husam Study/Documents/GitHub/ECE51216-Project/SAT/uf20-91/"; // 1000 SAT
 
-    size_t numVars = 0;
-    vector<Clause> clauses;
+    path = "C:/Users/Husam Study/Documents/GitHub/ECE51216-Project/SAT/uf50-218/"; // 1000 SAT
+    path = "C:/Users/Husam Study/Documents/GitHub/ECE51216-Project/SAT/uuf50-218/"; // 1000 UNSAT
 
-    int a = Parse(argv[1], clauses, numVars);
-    vector<int> solution(numVars + 1, 0);
-    vector<vector<unsigned>> uncomplemented(numVars + 1);
-    vector<vector<unsigned>> complemented(numVars + 1);
-    int c = Parse_uncomp(clauses, uncomplemented, complemented);
-    vector<Clause> to_check = clauses;
-    auto start = std::chrono::high_resolution_clock::now();
-    int b = DPLL(clauses, solution, uncomplemented, complemented);
-    auto end = std::chrono::high_resolution_clock::now();
-    if (b == 0) {
-        // Satsified
-        // call clean_solution
-        clean_solution(solution);
-        cout << "\t\tRESULT:SAT" << endl;
-        // run_sat() - this prints output in proper format
-        print_solution(solution);
-        check(to_check, solution);
-        std::chrono::duration<double> elapsed = end - start;
-        cout << "Elapsed time: " << elapsed.count() << " seconds" << endl;
+    path = "C:/Users/Husam Study/Documents/GitHub/ECE51216-Project/SAT/uf75-325/"; // 100 SAT
+    path = "C:/Users/Husam Study/Documents/GitHub/ECE51216-Project/SAT/uuf75-325/"; // 100 UNSAT
+
+    path = "C:/Users/Husam Study/Documents/GitHub/ECE51216-Project/SAT/uf100-430/"; // 1000 SAT
+    path = "C:/Users/Husam Study/Documents/GitHub/ECE51216-Project/SAT/uuf100-430/"; // 1000 UNSAT
+
+    path = "C:/Users/Husam Study/Documents/GitHub/ECE51216-Project/SAT/uf125-538/"; // 100 SAT
+    path = "C:/Users/Husam Study/Documents/GitHub/ECE51216-Project/SAT/uuf125-538/"; // 100 UNSAT
+
+    path = "C:/Users/Husam Study/Documents/GitHub/ECE51216-Project/SAT/uf150-645/"; // 100 SAT
+    path = "C:/Users/Husam Study/Documents/GitHub/ECE51216-Project/SAT/uuf150-645/"; // 100 UNSAT
+
+    path = "C:/Users/Husam Study/Documents/GitHub/ECE51216-Project/SAT/uf175-753/"; // 100 SAT
+    path = "C:/Users/Husam Study/Documents/GitHub/ECE51216-Project/SAT/uuf175-753/"; // 100 UNSAT
+
+    path = "C:/Users/Husam Study/Documents/GitHub/ECE51216-Project/SAT/uf200-860/"; // 100 SAT
+    path = "C:/Users/Husam Study/Documents/GitHub/ECE51216-Project/SAT/uuf200-860/"; // 100 UNSAT
+
+    //path = "C:/Users/Husam Study/Documents/GitHub/ECE51216-Project/SAT/uf225-960/"; // 100 SAT
+    //path = "C:/Users/Husam Study/Documents/GitHub/ECE51216-Project/SAT/uuf225-960/"; // 100 UNSAT
+
+    //path = "C:/Users/Husam Study/Documents/GitHub/ECE51216-Project/SAT/uf250-1065/"; // 100 SAT
+    //path = "C:/Users/Husam Study/Documents/GitHub/ECE51216-Project/SAT/uuf250-1065/"; // 100 UNSAT
+
+
+
+
+
+    vector<string> paths;
+    //paths.push_back(path);
+    //paths.push_back("C:/Users/Husam Study/Documents/GitHub/ECE51216-Project/SAT/uf125-538/uf125-054.cnf");
+    for (const auto& entry : filesystem::directory_iterator(path)) {
+        paths.push_back(path + entry.path().filename().string());
     }
-    else {
-        // Unsatisfied
-        cout << "\t\tRESULT:UNSAT" << endl;
-        ++unsat;
-        std::chrono::duration<double> elapsed = end - start;
-        cout << "Elapsed time: " << elapsed.count() << " seconds" << endl;
-        //exit(3);
+    //for (const auto & a : paths) cout << a << endl;
+    //path += "uuf50-01.cnf";
+    //paths.push_back(path);
+
+    // for (string s : paths) {
+    //     cout << s << endl;
+    // }
+   // paths.push_back(path);
+    //paths.push_back("broken.cnf");
+
+    for (const string & p : paths) {
+        size_t numVars = 0;
+        //a//cout << p << endl;
+        vector<Clause> clauses;
+
+        int a = Parse(p, clauses, numVars);
+        vector<int> solution(numVars + 1, 0);
+        vector<vector<unsigned>> uncomplemented(numVars + 1);
+        vector<vector<unsigned>> complemented(numVars + 1);
+        int c = Parse_uncomp(clauses, uncomplemented, complemented);
+        vector<Clause> to_check = clauses;
+        auto start = std::chrono::high_resolution_clock::now();
+        int b = DPLL(clauses, solution, uncomplemented, complemented);
+        auto end = std::chrono::high_resolution_clock::now();
+        if (b == 0) {
+            // Satsified
+            // call clean_solution
+            clean_solution(solution);
+            //a//cout << "\t\tRESULT:SAT" << endl;
+            // run_sat() - this prints output in proper format
+            print_solution(solution);
+            check(to_check, solution);
+            std::chrono::duration<double> elapsed = end - start;
+            cout << "Elapsed time: " << elapsed.count() << " seconds" << endl;
+            elapsed_seconds.push_back(elapsed.count());
+        }
+        else {
+            // Unsatisfied
+            //a//cout << "\t\tRESULT:UNSAT" << endl;
+            ++unsat;
+            std::chrono::duration<double> elapsed = end - start;
+            cout << "Elapsed time: " << elapsed.count() << " seconds" << endl;
+            elapsed_seconds.push_back(elapsed.count());
+            //exit(3);
+        }
     }
+    cout << endl << endl << endl << endl;
+
+    ///for (double elapsed_second : elapsed_seconds) {
+    ///    cout << elapsed_second << endl;
+    ///}
 
     cout << " Fail # " << fail << endl;
     cout << " Unsat # " << unsat << endl;
     cout << " Sat # " << sat << endl;
+
+    if (!path.empty() && path.back() == '/') {
+        path.pop_back();
+    }
+
+    // Find last slash
+    size_t lastSlash = path.find_last_of("/\\");
+    std::string lastPart = path.substr(lastSlash + 1);
+
+    //std::cout << "Last part: " << lastPart << "\n"; // Outputs: uf20-91
+    lastPart += ".txt";
+
+    std::ofstream outFile(lastPart);
+    if (!outFile) {
+        std::cerr << "Failed to open file: " << lastPart << "\n";
+        return 1;
+    }
+
+    for (double val : elapsed_seconds) {
+        outFile << val << "\n";
+    }
+
+
 
     return 0;
 }
